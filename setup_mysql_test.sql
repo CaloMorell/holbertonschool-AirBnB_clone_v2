7e506d2153cd9fc478730d0c9cf546571037ed39
figue0122 <you@example.com>
2023-11-30 10:14:51 -0400

    add comment

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,4 @@
-"""Write a script that prepares a MySQL server for the project"""
-
+-- Write a script that prepares a MySQL server for the project
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
