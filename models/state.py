802b0f4b755aa994124146fbc7029d10c9457c5d
CaloMorell <oscarmorell07@yahoo.com>
2023-12-01 21:17:24 -0400

    Update State

--- models/state.py
+++ models/state.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from os import getenv
 from sqlalchemy.orm import relationship
-import models
-from models.city import City
-import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
+    __tablename__ = 'states'
 
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        ''' File Storage relationship '''
+        @property
+        def cities(self):
+            '''
+            returns the list of City instances
+            with state_id equals to the current State.id
+            '''
+            from models import storage
+            from models.city import City
 
-    __tablename__ = "states"
+            city_list = []
+            city_dict = storage.all(City)
 
-    name = Column(String(128), nullable=False)
-    cities = relationship("Ciudad", backref="estado", cascade="all, delete-orphan")
-
-
-    @property
-    def cities(self):
-        """Atributo getter para ciudades en FileStorage"""
-        city_list = []
-        all_cit = models.storage.all(City)
-        for city in models.storage.all(models.Ciudad).values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+            for city in city_dict.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
