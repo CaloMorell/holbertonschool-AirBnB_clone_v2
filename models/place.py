ec69d256f6046b5c0ff60a42d8ba5c187e0a536d
CaloMorell <oscarmorell07@yahoo.com>
2023-12-01 22:22:28 -0400

    Update Place

--- models/place.py
+++ models/place.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float
-from sqlalchemy.schema import ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.amenity import Amenity
+
+place_amenity = Table('place_amenity', Base.metadata,
+                    Column('place_id', String(60),
+                    ForeignKey('places.id'),
+                    primary_key=True, nullable=False),
+                    Column('amenity_id', String(60),
+                    ForeignKey('amenities.id'),
+                    primary_key=True, nullable=False),
+                )
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    
     __tablename__ = 'places'
 
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
@@ -20,3 +30,42 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+    amenity_ids = []
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship(
+            'Review', cascade='all, delete', backref='place')
+        amenities = relationship(
+            'Amenity', secondary=place_amenity, viewonly=False,
+            backref='place_amenities')
+    else:
+        @property
+        def reviews(self):
+            '''FileStorage relationship between Place and Review'''
+            from models import storage
+            from models.review import Review
+
+            review_list = []
+            review_dict = storage.all(Review)
+            for review in review_dict.values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
+
+        @property
+        def amenities(self):
+            '''
+            Returns the list of `Amenity` instances
+            based on the attribute `amenity_ids` that
+            contains all `Amenity.id` linked to the Place
+            '''
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            '''
+            handles append method for adding an
+            Amenity.id to the attribute amenity_ids
+            '''
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
